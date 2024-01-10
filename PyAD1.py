908310a326fc51b09773afb443500e10e0fb1b9d
McDeco <153613402+McDeco8@users.noreply.github.com>
2024-01-10 14:39:51 +0530

    Add files via upload

---
+++ PyAD1.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("Hello, World!")
+
+
+# In[1]:
+
+
+a = 5
+b = 3
+sum_result = a + b
+print("the sum of given number is: ",sum_result)
+
+
+# In[3]:
+
+
+a = 5
+b = 3
+sub_result = a - b
+print("the difference of given number is: ",sub_result)
+
+
+# In[5]:
+
+
+a = 5
+b = 3
+mult_result = a * b
+print(f"The multiplication of given numbers is: " ,mult_result)
+
+
+# In[4]:
+
+
+a = 45
+b = 3
+div_result = a / b
+print(f"The division of given numbers is: " ,div_result)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
